cbe7f20b19a273b4c5d647ead0ef1f41601ced4d
Glitch (sky-quilled-lung) <none>
2021-08-17 11:03:35 +0000

    🚁🚟 Checkpoint ./komutlar/yardım.js:16006939/174

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,7 +6,7 @@ module.exports = {
   $textSplit[$sendMessage[
 \`
 \`
-
+$textSplit[$sendMessage[$image https://media.discordapp.net/attachments/837255566979235900/864783974807502888/standard_1.gif
 \`Tepkiye basarak yardım menüsünü görebilirsiniz.\`
 ;yes]; ]
   `
