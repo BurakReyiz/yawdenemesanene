a3cc836add2e03bcec5f470a1b11f2495742963a
Glitch (sky-quilled-lung) <none>
2021-08-17 10:52:24 +0000

    🐥🕶 Checkpoint ./komutlar/snipe.js:16006939/1 ./komutlar/yardım.js:16006939/4

--- komutlar/snipe.js
+++ komutlar/snipe.js
@@ -9,6 +9,6 @@ module.exports = {
   ]
 $color[$getServerVar[hex]]
 $onlyIf[$getServerVar[zerotwo]!=;:x: | **Bir mesaj kaydedilmemiş.**]
-$onlyIf[$hasPerms[$authorID;admin]!=false;Bukomutu kullanabilmek için **Yönetici** yetkisine sahip olmalısın]
+$onlyIf[$hasPerms[$authorID;admin]!=false;Bu komutu kullanabilmek için **Yönetici** yetkisine sahip olmalısın]
   `
 }
