59da09a01911184d9f6c12d09c004f555208858b
Glitch (sky-quilled-lung) <none>
2021-08-17 11:10:01 +0000

    Rewound to commit a3cc836add2e03bcec5f470a1b11f2495742963a + 0 edits

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,9 +3,10 @@ module.exports = {
   aliases:"help",
   code:`
  $reactionCollector[$splitText[1];$authorID;30s;✅;onay;yes]
+  $textSplit[$sendMessage[
 \`
 \`
-$image[https://media.discordapp.net/attachments/837255566979235900/864783974807502888/standard_1.gif]
+
 \`Tepkiye basarak yardım menüsünü görebilirsiniz.\`
 ;yes]; ]
   `
