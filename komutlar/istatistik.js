a76b51dbf66289717c13f2db05630f892b3681ef
Glitch (sky-quilled-lung) <none>
2021-08-19 12:09:09 +0000

    🎂👓 Checkpoint ./komutlar/istatistik.js:11412426/483:16006939/76

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -5,27 +5,27 @@ code:`
 $author[$userTag;$authorAvatar]
 $description[Selam **$userTag[$clientID]** İşte Mixden'nin İstatistikleri
 
-•🔧Sahibim: **$userTag[$botOwnerID]**
+$customEmoji[RenkliKral] • Sahibim: **$userTag[$botOwnerID]**
 
-•✌️ Komut sayım: **$commandsCount**
+ • Komut Sayım: **$commandsCount**
 
-•🌐 Toplam kanal sayım: **$allChannelsCount**
+ • Toplam Kanal Syım: **$allChannelsCount**
 
-•🌐 Toplam yazı kanalı: **$allChannelsCount[text]**
+ • Toplam Yazı Kanalı: **$allChannelsCount[text]**
 
-•🌐 Toplam ses kanal: **$allChannelsCount[voice]**
+•🌐 Toplam Ses Kanal: **$allChannelsCount[voice]**
 
-•🌐 Toplam kategori sayısı: **$allChannelsCount[category]**
+•🌐 Toplam Kategori Sayısı: **$allChannelsCount[category]**
 
-•👤 Toplam kullanıcı sayım: **$allMembersCount**
+•👤 Toplam Kullanıcı Sayım: **$allMembersCount**
 
-•✨ Toplam sunucu sayım: **$serverCount**
+•✨ Toplam Sunucu Sayım: **$serverCount**
 
-•🍓 Hesap kuruluş tarihim: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$clientID;time];years;Yıl;-1];months;Ay;-1];weeks; Hafta;-1];day;Gün;-1];hours;Saat;-1];minutes;Dakika;-1];and;Ve;-1];seconds;Saniye;-1] ** önce
+•🍓 Oluşturma Tarihim: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$clientID;time];years;Yıl;-1];months;Ay;-1];weeks; Hafta;-1];day;Gün;-1];hours;Saat;-1];minutes;Dakika;-1];and;Ve;-1];seconds;Saniye;-1] ** önce
 
-•🍉 Uptime sürem: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$djseval[client.uptime;yes];time];hours;saat;-1];minutes;dakika;-1];seconds;saniye;-1];hour;saat;-1];minute;dakika;-1];second;saniye;-1];and;ve;-1]**
+$customEmoji[yukleniyo] Uptime Sürem: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$djseval[client.uptime;yes];time];hours;saat;-1];minutes;dakika;-1];seconds;saniye;-1];hour;saat;-1];minute;dakika;-1];second;saniye;-1];and;ve;-1]**
 
-•🍒 Pingim: **$botPing**
+$customEmoji[yesilyuklen] • Pingim: **$botPing** 
 
 •🍏 Linkler: ** [Destek sunucum](https://discord.gg/QtffYC2Ubd) , [Davet linkim](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=8 ) **]
 $footer[Mixden İstatistik Menüsü;$authorAvatar]
