b1a91eecb9f10715b9c082487e924ff0b881346a
Glitch (sky-quilled-lung) <none>
2021-08-19 19:14:34 +0000

    🌷🔮 Checkpoint ./komutlar/hgbb-sistemi.js:11412426/154

--- komutlar/hgbb-sistemi.js
+++ komutlar/hgbb-sistemi.js
@@ -2,13 +2,13 @@ module.exports = {
         name: "hgbb-sistemi",
         code: `$author[HGBB Sistemi;$authorAvatar]
         $description[
-        ======================================================
+        ======================================
         
         **HGBB Sistemini açmak için:** \`$getServerVar[prefix]hgbb aç #kanal\`
         **HGBB Sistemini kapatmak için:** \`$getServerVar[prefix]hgbb kapat\`
         **HGBB Yazılarına resim eklemek için:** \`$getServerVar[prefix]hgbb resim <resim-url>\`
          
-        ======================================================
+        ======================================
         
         **HG Mesajını değiştirmek için:** \`$getServerVar[prefix]hgtxt düzenle <yeni-hg-mesajı>\`
         **HG Mesajını sıfırlamak için:** \`$getServerVar[prefix]hgtxt sıfırla\`
@@ -16,13 +16,13 @@ module.exports = {
         **BB Mesajını değiştirmek için:** \`$getServerVar[prefix]bbtxt düzenle <yeni-bb-mesajı>\`
         **BB Mesajını sıfırlamak için:** \`$getServerVar[prefix]bbtxt sıfırla\`
         
-        ======================================================
+        ======================================
         
         **-etiket- -> <@$authorID>**
         **-isim- -> $username**
         **-isimt- -> $usertag**
         
-        ======================================================
+        ======================================
         ]
         $color[$getServerVar[hex]]
         $thumbnail[$serverIcon]
