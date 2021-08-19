55e614e9c0e29989890f86d5aaebc5fa8798fd45
Glitch (sky-quilled-lung) <none>
2021-08-19 16:32:33 +0000

    👍🎇 Checkpoint ./komutlar/destek.js:16006939/1202 ./komutlar/toppre.js:16006939/510

--- komutlar/destek.js
+++ komutlar/destek.js
@@ -1,1 +1,16 @@
-module.export
+module.exports = {
+name:"destek",
+code:`
+$title[Mixden Destek]
+$description[
+customEmoji[sevimlikiz]**Selam Bir Komuttamı Takıldınız Veya Bir Hatamı Gördünüz? Sizi Destek Sunucumuza Davet Ediyorum!
+Destek Sunucumuzda 7/24 Hizmet Vardır. Sunucuya Girdikden Sonra Ticket Açıp Sorununuzu Anlata Bilirsiniz!**
+
+
+__Desteğe Geçmek İçin Sunucumuza Katılın__
+[Destek Sunucumuz](https://discord.gg/QtffYC2Ubd)
+]
+$footer[Mixden Destek Sunucusu]
+`
+}
+
