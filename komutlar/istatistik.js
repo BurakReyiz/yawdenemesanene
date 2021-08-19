60c205bdb99cdb9a505f7af1eef9605ecae09749
Glitch (sky-quilled-lung) <none>
2021-08-19 12:33:03 +0000

    🌨🍼 Checkpoint ./komutlar/istatistik.js:11412426/153

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -3,25 +3,25 @@ name:"i",
 aliases:["istatistik","istatistiks","stats","stat"],
 code:`
 $author[$userTag;$authorAvatar]
-$description[Selam **$userTag[$clientID]** İşte Mixden'nin İstatistikleri
+$description[Selam \`$userTag[$clientID]\` İşte Mixden'nin İstatistikleri
 
-$customEmoji[sari1] • Sahibim: **$userTag[$botOwnerID]**
+$customEmoji[sari1] • Sahibim: \`$userTag[$botOwnerID]\`
 
-$customEmoji[kirmiziates] • Toplam Kullanıcı Sayım: **$allMembersCount**
-$customEmoji[discord] • Toplam Sunucu Sayım: **$serverCount**
+$customEmoji[kirmiziates] • Toplam Kullanıcı Sayım: \`$allMembersCount\`
+$customEmoji[discord] • Toplam Sunucu Sayım: \`$serverCount\`
 
-$customEmoji[gokkusagi] • Komut Sayım: **$commandsCount**
+$customEmoji[gokkusagi] • Komut Sayım: \`$commandsCount\`
 
-$custom[emoji_99] • Toplam Kanal Sayım: **$allChannelsCount**
-$customEmoji[ayicik] • Toplam Yazı Kanalı: **$allChannelsCount[text]**
-$customEmoji[portakal] • Toplam Ses Kanal: **$allChannelsCount[voice]**
-$customEmoji[ • Toplam Kategori Sayısı: **$allChannelsCount[category]**
+$customEmoji[emoji_99] • Toplam Kanal Sayım: \`$allChannelsCount\`
+$customEmoji[ayicik] • Toplam Yazı Kanalı: \`$allChannelsCount[text]\`
+$customEmoji[portakal] • Toplam Ses Kanal: \`$allChannelsCount[voice]\`
+$customEmoji[blob] • Toplam Kategori Sayısı: \`$allChannelsCount[category]\`
 
-$customEmoji[sabit] • Oluşturma Tarihim: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$clientID;time];years;Yıl;-1];months;Ay;-1];weeks; Hafta;-1];day;Gün;-1];hours;Saat;-1];minutes;Dakika;-1];and;Ve;-1];seconds;Saniye;-1] ** önce
+$customEmoji[sabit] • Oluşturma Tarihim: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$clientID;time];years;Yıl;-1];months;Ay;-1];weeks; Hafta;-1];day;Gün;-1];hours;Saat;-1];minutes;Dakika;-1];and;Ve;-1];seconds;Saniye;-1]\`
 
-$customEmoji[yukleniyo] • Uptime Sürem: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$djseval[client.uptime;yes];time];hours;saat;-1];minutes;dakika;-1];seconds;saniye;-1];hour;saat;-1];minute;dakika;-1];second;saniye;-1];and;ve;-1]**
+$customEmoji[yukleniyo] • Uptime Sürem: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$djseval[client.uptime;yes];time];hours;Saat;-1];minutes;Dakika;-1];seconds;Saniye;-1];hour;Saat;-1];minute;Dakika;-1];second;Saniye;-1];and;ve;-1]\`
 
-$customEmoji[yesilyuklen] • Pingim: **$botPing** 
+$customEmoji[yesilyuklen] • Pingim: \`$ping.ms\`
 
 $customEmoji[zil] • Kütüphanem: \`AOI.js\`
 
