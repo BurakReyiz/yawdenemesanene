948dc94af2441415f934b4f81390dbb7b4dac62f
Glitch (sky-quilled-lung) <none>
2021-08-19 19:02:59 +0000

    🚗🐑 Checkpoint ./komutlar/müzik.js:11412426/113

--- komutlar/müzik.js
+++ komutlar/müzik.js
@@ -1,21 +1,21 @@
 module.exports = {
 name:"müzik",
 code:`$title[<a:684883779424485385:855893751535501352> Mixden Müzik Sistemi <a:684883779424485385:855893751535501352>]
-        $description[
-        ==========================     
-    <a:kilic:839635834076659712> **Şarkıyı Başlatmak İçin:** \`$getServerVar[prefix]çal\`
-    <a:wumpus_klavye:858362871553851422> **Şarkı Sırasını Açmak İçin:** \`$getServerVar[prefix]sıralama\`      
-        ==========================  
-    <a:yukleniyor:848925030125797396> **Şarkıyı Durdurmak İçin:** \`$getServerVar[prefix]durdur\`
-    <a:kafa_sallan_anime:839636622210629642> **Şarkıyı Devam Ettirmek İçin:** \`$getServerVar[prefix]devam\`        
-        ==========================   
-    <a:renkli_koyun:839636458780229632> **Şarkıyı Tekrarlamak İçin:** \`$getServerVar[prefix]tekrarla\`
-    <a:wumpus_nitro:858379878105743361> **Şarkıyı Geçmek İçin:** \`$getServerVar[prefix]geç\`
-        ==========================
-     <a:pepe_exit:858431557076647962> **Şarkıyı Bitirmek İçin Ses 
-      Kanalından Çıkmanız Yeterlidir**
-        ]
-        $color[$getServerVar[hex]]
-        $thumbnail[$serverIcon]
+$description[
+==========================     
+<a:kilic:839635834076659712> **Şarkıyı Başlatmak İçin:** \`$getServerVar[prefix]çal\`
+<a:wumpus_klavye:858362871553851422> **Şarkı Sırasını Açmak İçin:** \`$getServerVar[prefix]sıralama\`      
+==========================  
+<a:yukleniyor:848925030125797396> **Şarkıyı Durdurmak İçin:** \`$getServerVar[prefix]durdur\`
+<a:kafa_sallan_anime:839636622210629642> **Şarkıyı Devam Ettirmek İçin:** \`$getServerVar[prefix]devam\`        
+==========================   
+<a:renkli_koyun:839636458780229632> **Şarkıyı Tekrarlamak İçin:** \`$getServerVar[prefix]tekrarla\`
+<a:wumpus_nitro:858379878105743361> **Şarkıyı Geçmek İçin:** \`$getServerVar[prefix]geç\`
+==========================
+<a:pepe_exit:858431557076647962> **Şarkıyı Bitirmek İçin Ses 
+Kanalından Çıkmanız Yeterlidir**
+]
+$color[$getServerVar[hex]]
+$thumbnail[$serverIcon]
 `
 }
