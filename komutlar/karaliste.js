86454cf3ae7739a1b4cdbdfc281977d73d953a90
Glitch (sky-quilled-lung) <none>
2021-08-20 19:07:36 +0000

    👛👏 Checkpoint ./komutlar/karaliste.js:16006939/101

--- komutlar/karaliste.js
+++ komutlar/karaliste.js
@@ -2,9 +2,10 @@ module.exports = {
 name:"karaliste",
 code:`
 $if[$message[1]==ver]
-✔️ <@$message[1]> adlı kişiyi **$getGlobalUserVar[ksebep;$message[1]] ** sebebinden kara listeye aldımke
+✔️ <@$message[1]> adlı kişiyi **$getGlobalUserVar[ksebep;$message[1]] ** sebebinden kara listeye aldım
 $setGlobalUserVar[karaliste;yes;$message[1]]
 $setGlobalUserVar[ksebep;$message[2] $message[3] $message[4] $message[5] $message[6] $message[7] $message[8] ;$message[9] $message[10];$message[1]]
+$onlyForIDs[$botOwnerID;{title;Karalisteye Yalnız Sahibim Alabilir!}
 $onlyIf[$message[2]!=;Sebep gir]
 $onlyIf[$isNumber[$message[1]]==true;Bu Bir İd'ye Benzemiyor!]
 $onlyIf[$message[1]!=;Kullanıcının İd'sini Gir!]
@@ -17,5 +18,5 @@ $onlyForIDs[$botOwnerID;{title:Karalisteye Yalnızca Sahibim Alabilir!}
 $onlyIf[$getGlobalUserVar[karaliste;$message[1]]==yes;Bu üye karalistede değil]
 $onlyIf[$isNumber[$message[1]]==true;Bu Bir İd'ye Benzemiyor!]
 $onlyIf[$message[1]!=;Kullanıcının İd'sini Gir!]
-`
+]]`
 }
