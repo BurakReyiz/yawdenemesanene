21ba5c94a5cef1b5693598b670bf063ff10d2517
Glitch (sky-quilled-lung) <none>
2021-08-20 08:36:28 +0000

    💑📖 Checkpoint ./rodzen.js:16006939/1180

--- rodzen.js
+++ rodzen.js
@@ -150,24 +150,6 @@ $onlyIf[$message!=;**Lütfen Bir Mesaj Gir!**]`
   $argsCheck[>1;Süre Belirtmen Gerekiyor. Doğru kullanım : $getServerVar[prefix]kilitle #kanal süre]
 `
   })
-module.exports = ({
-  name:"kilit-aç",
-  code:`
-  $modifyChannelPerms[$mentionedChannels[1;yes];+sendmessages;$guildID]
-  $title[$username[$clientID] Kanal Kilit açma sistemi]
-  $description[Başarıyla Kanalın kilidi açıldı
-  
-  Kilidi açılan Kanal : <#$mentionedChannels[1;yes]>
-  Kilidi açan Yetkili : $userTag[$authorID]]
-  $color[303136]
-  $thumbnail[$authorAvatar]
-  $useChannel[$channelID]
-  $onlyIf[$isNumber[$noMentionMessage[1]]==false;Lütfen geçerli bir kanal etiketle.]
-  $onlyBotPerms[managechannels;{title:Hata}{description:Bu komutu kullanabilmek için sunucuda **Kanalları Yönet** yetkisine sahip olmam gerekiyor.}{color:RED}]
-  $onlyPerms[managechannels;{title:Hata}{description:Bu komutu kullanabilmek için sunucuda **Kanalları Yönet** yetkisine sahip olman gerekiyor.}{color:RED}]
-  $argsCheck[>1;Lütfen Bir Kanal Etiketle. \`!!kilit-aç #$channelName[$channelID]\` gibi]
-`
-  })
 bot.command({
 name:"saas-aç",
 code:`
