213ef4244d0f21fa62cc3f31d9ad2e5515277fe7
Glitch (sky-quilled-lung) <none>
2021-08-20 11:06:33 +0000

    🐊🌗 Checkpoint ./komutlar/bakımkapat.js:16006939/980 ./komutlar/yardım.js:16006939/1334

--- komutlar/bakımkapat.js
+++ komutlar/bakımkapat.js
@@ -1,11 +1,11 @@
 module.exports = {
   name:"bakımkapat",
   code:`
-Bakım Modu Kapatılmıştır $customEmoji[yesiltik]
+Bakım Modu Kapatılmıştır $customEmoji[yesiltik]{author:$userTag:$authorAvatar}
 Bütün Komutlar Kullanıma Açılmıştır. $customEmoji[yukleniyor2]
 $setGlobalUserVar[bakım;hayır;310301496824561666]
 $onlyForIDs[310301496824561666;Geliştiricim Değilsin]
-{thumbnail:[$authorAvatar]} {color:$getServerVar[hex]}
+{footer:Mixden Bakım Sistemi}
 
 `
 }
