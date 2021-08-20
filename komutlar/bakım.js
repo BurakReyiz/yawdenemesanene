991427252a092ae6849325a4e8304ea454bbc2b0
Glitch (sky-quilled-lung) <none>
2021-08-20 09:45:47 +0000

    🏢🦃 Checkpoint ./komutlar/bakım.js:16006939/17

--- komutlar/bakım.js
+++ komutlar/bakım.js
@@ -3,13 +3,13 @@ name:"bakım",
 code:`
 $if[$message[1]==aç]
 ✔️ Bakım açıldı Artık Komutlar Kullanılamaz Durumda
-$setVar[bakım;yes]
-$onlyIf[$getVar[bakım]==no;⚠️ Zaten bakım'dayım] 
+$setVar[bakım;no]
+$onlyIf[$getVar[bakım]==yes;⚠️ Zaten bakım'dayım] 
 $endif
 $if[$message[1]==kapat]
 ✔️ Bakım kapatıldı artık komutlarım kullanılabilir
 $setVar[bakım;no]
-$onlyIf[$getVar[bakım]==no;]⚠️ Zaten bakım'da değilim]
+$onlyIf[$getVar[bakım]==yes;]⚠️ Zaten bakım'da değilim]
 $endif
 $onlyIf[$checkContains[$toLowercase[$message[1]];aç;kapat]==true;⚠️ **aç** veya **kapat** argumanlarını kullan]
 $onlyIf[$authorID==$botOwnerID;Bunu Yalnızca Sahibim Kullanabilir!]
