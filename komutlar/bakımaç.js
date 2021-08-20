3acad78f341212f9ad3e26c278779e89cb783baa
Glitch (sky-quilled-lung) <none>
2021-08-20 13:11:49 +0000

    🚙🌔 Checkpoint ./komutlar/bakımaç.js:16006939/1224

--- komutlar/bakımaç.js
+++ komutlar/bakımaç.js
@@ -1,15 +1,10 @@
 module.exports = {
   name:"bakım-aç",
   code:`
-$author[$usertag;$authorAvatar]
-$if[$message[1]==;aç]
-Bakım Modu Açılmıştır $customEmoji[yesiltik]
-Bütün Komutlar Kullanıma Kapatılmıştır. $customEmoji[yukleniyor2]
-$customEmoji[pepe_exit]Kapatmak İçin $getServerVar[prefix]bakım-kapat Yazman Yeterli Kurucum $customEmoji[siyahkraltaci]]
-$setVar[bakım;yes]
-$onlyIf[getVar[bakım]==no;Zaten Bakımdayım
-$endif
-$setGlobalUserVar[bakım;evet;310301496824561666;654741976327127080]
-$onlyForIDs[310301496824561666;654741976327127080;Geliştiricim Değilsin]
+$author[$userTag;$authorAvatar]
+$description[Bakım Modu Açılmışdır $customEmoji[yesiltik]
+Bütün Komutlar Kullanıma Kapanmışdır. $customEmoji[yukleniyor2]]
+$onlyIf[$getVar[bakım]==yes;:warning: Üzgünüm bakımdayım]
+$onlyForIDs[310301496824561666;Geliştiricim Değilsin]
 $footer[Mixden Bakım Sistemi]
-`}
+`} 
