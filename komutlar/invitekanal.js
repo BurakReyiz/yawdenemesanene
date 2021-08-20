f2b0caeab0c8d72bfa3370cbb5340c95d1827165
Glitch (sky-quilled-lung) <none>
2021-08-20 17:49:10 +0000

    🌧🐂 Checkpoint ./komutlar/invitekanal.js:16006939/45 ./komutlar/ticket-sistemi.js:16006939/541

--- komutlar/invitekanal.js
+++ komutlar/invitekanal.js
@@ -4,6 +4,6 @@ code:`
 İnvite Kanalı <#$mentionedChannels[1]> Olarak Ayarlandı
 $setServerVar[invite;$mentionedChannels[1]]
 $onlyPerms[admin;Bunun İçin \`Yönetici\` İznin Olmalı]
-$onlyIf[$mentionedChannels[1]!=; Bir Kanal Etiketle]$onlyIf[$message!=; **Kullanım:** !invite-kanal #invite ]
+$onlyIf[$mentionedChannels[1]!=; Bir Kanal Etiketle]$onlyIf[$message!=; **Örnek Kullanım:** $getServerVar[prefix]invite-kanal #kanal ]
 `
 }
--- komutlar/ticket-sistemi.js
+++ komutlar/ticket-sistemi.js
@@ -1,17 +1,18 @@
 module.exports = {
   name:"ticket-sistemi",
   code:`
-  $author[TİCKET SİSTEMİ;$authorAvatar]
+  $author[Mixden Ticket Sistemi;$authorAvatar]
         $description[
-        **Ticket kanalı ayarlamak için:** \`$getServerVar[prefix]ticket-kanal ayarla #kanal\`
-        **Ticket kanalını sıfırlamak için:** \`$getServerVar[prefix]ticket-kanal sıfırla\`
-        -=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-
-        **Ticket kategorisini ayarlamak için:** \`$getServerVar[prefix]ticket-kategori KATEGORİ_ID\`
-        **Ticket kategorisini sıfırlamak için:** \`$getServerVar[prefix]ticket-kategori sıfırla\`
-        -=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-=0=-
-        **Ticket açmak için** \`Ayarlanan ticket kanalına mesaj yazın\`
-        **Ticket kapatmak için:** \`$getServerVar[prefix]ticket kapat\`
+**Ticket kanalı ayarlamak için:** \`$getServerVar[prefix]ticket-kanal ayarla #kanal\`
+**Ticket kanalını sıfırlamak için:** \`$getServerVar[prefix]ticket-kanal sıfırla\`
+=================
+**Ticket kategorisini ayarlamak için:** \`$getServerVar[prefix]ticket-kategori KATEGORİ_ID\`
+**Ticket kategorisini sıfırlamak için:** \`$getServerVar[prefix]ticket-kategori sıfırla\`
+=================
+**Ticket açmak için** \`Ayarlanan ticket kanalına $getServerVar[prefix]ticket-aç yazın\`
+**Ticket kapatmak için:** \`$getServerVar[prefix]ticket kapat\`
         ]
+        $footer[Destek'e İhtiyacınız Varsa $getServerVar[prefix]destek Yazmanız Yeterlidir.]
         $color[$getServerVar[hex]]
         $thumbnail[$serverIcon]  
 
