a0c8249eae235a503871dbd6b2289abb1974efe5
Glitch (sky-quilled-lung) <none>
2021-08-20 12:27:02 +0000

    🙅👻 Checkpoint ./komutlar/afk.js:16006939/2 ./komutlar/25milse.js:16006939/52

--- komutlar/25milse.js
+++ komutlar/25milse.js
@@ -6,7 +6,7 @@ $deletecommand
 $title[25 mile's]
 $image[https://vacefron.nl/api/water?text=$message]
 $footer[$username[$authorID]#$discriminator[$authorID]]
-$onlyIf[$getGlobalUserVar[bakım;310301496824561666]!=evet;:lock: Bakım Modu Açık Lütfen Daha Sonra Tekrar Deneyiniz]
+$onlyIf[$Var[bakım]==no;:lock: Bakım Modu Açık Lütfen Daha Sonra Tekrar Deneyiniz]
 `
 }
 
