cc8e51f6461fccf9a5c20f5442ad9f892a3a821c
Glitch (sky-quilled-lung) <none>
2021-08-20 11:17:50 +0000

    🖖🌘 Checkpoint ./komutlar/bakımkapat.js:16006939/2024 ./komutlar/bakımaç.js:16006939/866

--- komutlar/bakımaç.js
+++ komutlar/bakımaç.js
@@ -1,11 +1,11 @@
 module.exports = {
   name:"bakımaç",
   code:`
-Bakım Modu Açılmıştır $customEmoji[yesiltik]
+$author[$usertag;$authorAvatar]
+$description[Bakım Modu Açılmıştır $customEmoji[yesiltik]
 Bütün Komutlar Kullanıma Kapatılmıştır.$customEmoji[yukleniyor2]
-$customEmoji[pepe_exit]Kapatmak İçin $getServerVar[prefix]bakımkapat Yazman Yeterli Kurucum $customEmoji[siyahkraltaci]
+$customEmoji[pepe_exit]Kapatmak İçin $getServerVar[prefix]bakımkapat Yazman Yeterli Kurucum $customEmoji[siyahkraltaci]]
 $setGlobalUserVar[bakım;evet;310301496824561666]
 $onlyForIDs[310301496824561666;Geliştiricim Değilsin]
-$color[hex]]
-`
-}
+$footer[Mixden Bakım Sistemi] $color[Orange]
+`}
--- komutlar/bakımkapat.js
+++ komutlar/bakımkapat.js
@@ -1,11 +1,10 @@
 module.exports = {
   name:"bakımkapat",
   code:`
-Bakım Modu Kapatılmıştır $customEmoji[yesiltik]{author:$userTag:$authorAvatar}
-Bütün Komutlar Kullanıma Açılmıştır. $customEmoji[yukleniyor2]
+$author[$userTag;$authorAvatar]
+$description[Bakım Modu Kapatılmıştır $customEmoji[yesiltik]
+Bütün Komutlar Kullanıma Açılmıştır. $customEmoji[yukleniyor2]]
 $setGlobalUserVar[bakım;hayır;310301496824561666]
 $onlyForIDs[310301496824561666;Geliştiricim Değilsin]
-{footer:Mixden Bakım Sistemi}
-
-`
-}
+$footer[Mixden Bakım Sistemi]
+`} 
