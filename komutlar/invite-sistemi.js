4ae7f1904c207236a42fb600f95787ae07387e2d
Glitch (sky-quilled-lung) <none>
2021-08-20 17:26:40 +0000

    🦄🚕 Checkpoint ./komutlar/invite-sistemi:16006939/19 ./komutlar/invite-sistemi.js:16006939/1142

---
+++ komutlar/invite-sistemi.js
@@ -0,0 +1,17 @@
+module.exports = {
+name:"invite-sistemi",
+code:`$title[Mixden İnvite Sistemi]
+$description[
+==========================     
+<a:kilic:839635834076659712> **Birisinin Davetinin Sizin Seçdiğiniz Kanalda Görünmesi İçin:** \`$getServerVar[prefix]invite-kanal #kanal\`
+<a:wumpus_klavye:858362871553851422> **İnvite Sistemini Kapatmak İçin:** \`$getServerVar[prefix]invite-kapat\`      
+==========================  
+$customEmoji[sabit]Not : **İnvite Sisteminde Ayarladığınız Kanalda Hem Davet Eden Kişiyi Hem Giren Kişiyi Hemde Davet Edip Çıkan
+Kişiyi Göstermektedir.**
+==========================   
+]
+$color[$getServerVar[hex]]
+$thumbnail[$serverIcon]
+$footer[Destek Sunucumuza Gelebilmek İçin $getServerVar[prefix]destek Yazmanız Yeterlidir.]
+`
+}
