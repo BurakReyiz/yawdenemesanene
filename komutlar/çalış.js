86b6ebbf9a0dfe6334a8f95ae10652ff066acc36
Glitch (sky-quilled-lung) <none>
2021-08-21 12:00:33 +0000

    🙉❤️ Checkpoint ./komutlar/çalış.js:16006939/302

--- komutlar/çalış.js
+++ komutlar/çalış.js
@@ -1,9 +1,9 @@
 module.exports = {
         name: "çalış",
         code: `$author[$usertag Çalıştı.]
-        $description[$randomText[Polis olarak;Doktor olarak;Mimar olarak;Yazılımcı olarak] çalıştın ve $random[75;1000] $getServerVar[pbirim] kazandın.]
+        $description[$randomText[Manavcı olarak;Projeci olarak;Elektirikçi olarak;Youtuber olarak;Kasap olarak;Marketçi olarak;Büfeci olarak;Simitçi olarak;Kantinci olarak;Çiftçi olarak;Mühendis olarak;Polis olarak;Doktor olarak;Mimar olarak;Yazılımcı olarak] çalıştın ve $random[75;500] $getServerVar[pbirim] kazandın.]
         $color[303136]
-        $setUserVar[para;$sum[$random[75;150];$getUserVar[para;$authorID]];$authorID]
+        $setUserVar[para;$sum[$random[75;400];$getUserVar[para;$authorID]];$authorID]
         $cooldown[300s;:x: | 5 dakika beklemelisin.]
         
         `
