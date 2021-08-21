1b95597cb054733a80e4510b8719a4a833a13a51
Glitch (sky-quilled-lung) <none>
2021-08-21 12:56:41 +0000

    🎮📫 Checkpoint ./rodzen.js:11412426/15:16006939/184 ./komutlar/cüzdan.js:11412426/99:16006939/264

--- komutlar/cüzdan.js
+++ komutlar/cüzdan.js
@@ -2,7 +2,8 @@ module.exports = {
         name: "cüzdan",
         code: `$author[$userTag[$mentioned[1;yes]]'in Cüzdanı.]
         $description[
-        $addField[$userTag[$mentioned[1;yes]]'in Kullanıcının Cebinde $getServerVar[pbirim] $getUserVar[para;$mentioned[1;yes]];Para Var]
+        $addField[Kullanıcının Cebinde $getServerVar[pbirim] $getUserVar[para;$mentioned[1;yes]];**Kadar $getServerVar[pbirim] Para Var**]
+        $addField[]
         $color[RANDOM]
         $thumbnail[$userAvatar[$mentioned[1;yes]]]
         $onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların parası olmaz.]]
--- rodzen.js
+++ rodzen.js
@@ -341,7 +341,7 @@ $image[https://media.discordapp.net/attachments/837255566979235900/8647839748075
 $addField[$customEmoji[mavisiren] **Destek** (\`1\`);\`destek\`] 
 $addField[$customEmoji[kafa_sallan_anime2] **Müzik Komutları** (\`8\`);\`müzik, play, sıralama, resume, loop, skip, leave, sound\`]
 $addField[$customEmoji[dikkatet] **Koruma Komutlarım** (\`2\`);\`emoji-koruma, link-engel\`]
-$addField[$customEmoji[paracklerr] **Ekonomi Komutlarım** (\`14\`);\`ekonomi-yardım, kimlik, cüzdan, günlük, cf, market, para-birimi, para al/ver, para yatır/çek, para-gönder, çalış, satın-al, envanter\`]
+$addField[$customEmoji[paracklarr] **Ekonomi Komutlarım** (\`14\`);\`ekonomi-yardım, kimlik, cüzdan, günlük, cf, market, para-birimi, para al/ver, para yatır/çek, para-gönder, çalış, satın-al, envanter\`]
 $addField[$customEmoji[giveaway] **Eğlence Komutlarım** (\`8\`);\`howgay, 25miles, palyaço, biden, rte, trump, kaçcm, clyde\`]
 $addField[$customEmoji[asker] **Premium Komutlarım** (\`0\`);\`YAKINDA!\`]
 $addField[$customEmoji[kirmizi_siren] **Ayarlamalı Komutlarım** (\`12\`);\`hgbb-sistemi, mute-sistemi, ticket-sistemi, kayıt-sistemi, otorol, modlog, saas-aç, saas-kapat, tavsiye-log, invite-sistemi, level-sistemi\`]
@@ -351,7 +351,7 @@ $addField[**Toplam Komut** (\`$commandsCount\`);\`Moderasyon | Ayarlamalı | Kor
   $color[$getServerVar[hex]] 
   $thumbnail[$authorAvatar]`
 }) 
-//Hata nerde .d
+
 bot.awaitedCommand({
   name:"parzi",
   code:`
@@ -849,6 +849,7 @@ bot.variables({
   eklog:"",
   erol:"",
   krol:"",
+ KrediKartı:"",
   para:"0",
   banka:"0",
   rozet1:"false",
