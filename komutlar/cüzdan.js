7a50868609272de7af71302a67c99ae7dc12aa43
Glitch (sky-quilled-lung) <none>
2021-08-21 13:19:55 +0000

    🌯🍕 Checkpoint ./komutlar/cüzdan.js:11412426/76:16006939/190 ./komutlar/envanter.js:11412426/95:16006939/1

--- komutlar/cüzdan.js
+++ komutlar/cüzdan.js
@@ -1,8 +1,7 @@
 module.exports = {
         name: "cüzdan",
         code: `$author[$userTag[$mentioned[1;yes]]'in Cüzdanı.]
-        $description[**Kullanıcının Cebinde $getServerVar[pbirim] $getUserVar[para;$mentioned[1;yes]]
-        Cüzdanında $getServerVar[KrediKartı][1;]] Tane Kredi Kartı Var.**]
+        $description[**Kullanıcının Cebinde $getServerVar[pbirim]** **$getUserVar[para;[1;]] Kadar Para Var**]
         $color[RANDOM]
         $thumbnail[$userAvatar[$mentioned[1;yes]]]
         $onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların parası olmaz.]]
--- komutlar/envanter.js
+++ komutlar/envanter.js
@@ -2,13 +2,13 @@ module.exports = {
         name: "envanter",
         code: `
 $description[**Rozetler :** $replaceText[$replaceText[$getUserVar[rozet1;$mentioned[1;yes]];false;\`Yok\`;-1];true;🔱;-1] **|** $replaceText[$replaceText[$getUserVar[rozet2;$mentioned[1;yes]];false;\`Yok\`;-1];true;⚜️;-1]
-**Su :** $getUserVar[su;$mentioned[1;yes]]
-**Ayran :** $getUserVar[ayran;$mentioned[1;yes]]
-**Simit :** $getUserVar[simit;$mentioned[1;yes]]
-**Paran :** $getUserVar[para;$mentioned[1;yes]]
+**Su:** $getUserVar[su;$mentioned[1;yes]]
+**Ayran:** $getUserVar[ayran;$mentioned[1;yes]]
+**Simit:** $getUserVar[simit;$mentioned[1;yes]]
+**Paran:** $getServerVar[pbirim] $getUserVar[para;$mentioned[1;yes]]
 ]
 $thumbnail[$userAvatar[$mentioned[1;yes]]]
-$color[303136]
+$color[RANDOM]
 $onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların envanteri olmaz.]
         `
 }
