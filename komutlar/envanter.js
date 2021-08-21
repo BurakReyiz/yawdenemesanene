965232a4c3913ceb264921573080edf3c0b079fb
Glitch (sky-quilled-lung) <none>
2021-08-21 11:38:20 +0000

    🏘🚘 Checkpoint ./komutlar/ekonomi-yardım.js:16006939/389 ./komutlar/envanter.js:16006939/33

--- komutlar/envanter.js
+++ komutlar/envanter.js
@@ -1,14 +1,14 @@
 module.exports = {
         name: "envanter",
         code: `
-$descripion[**Rozetler:** $replaceText[$replaceText[$getUserVar[rozet1;$mentioned[1;yes]];false;\`Yok\`;-1];true;🔱;-1] **|** $replaceText[$replaceText[$getUserVar[rozet2;$mentioned[1;yes]];false;\`Yok\`;-1];true;⚜️;-1]
-**Su:** \`$getUserVar[su;$mentioned[1;yes]]\`
-**Ayran:** \`$getUserVar[ayran;$mentioned[1;yes]]\`
-**Simit:** \`$getUserVar[simit;$mentioned[1;yes]]\`
-**Paran:** \`$getUserVar[para;$mentioned[1;yes]]\`
+$description[**Rozetler:** $replaceText[$replaceText[$getUserVar[rozet1;$mentioned[1;yes]];false;\`Yok\`;-1];true;🔱;-1] **|** $replaceText[$replaceText[$getUserVar[rozet2;$mentioned[1;yes]];false;\`Yok\`;-1];true;⚜️;-1]
+**Su :** $getUserVar[su;$mentioned[1;yes]]
+**Ayran :** $getUserVar[ayran;$mentioned[1;yes]]
+**Simit :** $getUserVar[simit;$mentioned[1;yes]]
+**Paran :** $getUserVar[para;$mentioned[1;yes]]
 ]
 $thumbnail[$userAvatar[$mentioned[1;yes]]]
 $color[303136]
-        $onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların envanteri olmaz.]
+$onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların envanteri olmaz.]
         `
 }
