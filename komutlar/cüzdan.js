b70ae51d19ce03cdc72a9f28ecdbc5d5ce655f9e
Glitch (sky-quilled-lung) <none>
2021-08-21 13:07:56 +0000

    👬🌂 Checkpoint ./komutlar/cüzdan.js:11412426/99:16006939/103 ./komutlar/çalış.js:11412426/14

--- komutlar/cüzdan.js
+++ komutlar/cüzdan.js
@@ -1,9 +1,8 @@
 module.exports = {
         name: "cüzdan",
         code: `$author[$userTag[$mentioned[1;yes]]'in Cüzdanı.]
-        $description[
-        $addField[Kullanıcının Cebinde $getServerVar[pbirim] $getUserVar[para;$mentioned[1;yes]];**Kadar $getServerVar[pbirim] Para Var**]
-        $addField[]
+        $description[**Kullanıcının Cebinde $getServerVar[pbirim] $getUserVar[para;$mentioned[1;yes]]
+        Cüzdanında $getServerVar[KrediKartı][1;]] Tane Kredi Kartı Var.**]
         $color[RANDOM]
         $thumbnail[$userAvatar[$mentioned[1;yes]]]
         $onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların parası olmaz.]]
