dcf51a17450a847ca3d3c0a6e423dce67430ec04
Glitch (sky-quilled-lung) <none>
2021-08-21 13:54:22 +0000

    🍄👏 Checkpoint ./komutlar/banka.js:16006939/270

--- komutlar/banka.js
+++ komutlar/banka.js
@@ -1,4 +1,8 @@
 module.exports = {
 name: "banka",
 code: `
-$addField[🏦;\`$getUserVar[banka;$mentioned[1;yes]\`yes
+$addField[🏦;\Bankanda Şuan $getUserVar[banka;$mentioned[1;yes]] Paran Var]
+$color[hex]
+$onlyİf[isBot[mentioned[1;yes]]!=true;:x: | Dostum Bu Bir Bot Botların Parası Olmaz.]
+`
+}
