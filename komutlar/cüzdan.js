8d888c08470f12265c9c8d41401af5845c42ed45
Glitch (sky-quilled-lung) <none>
2021-08-21 13:31:01 +0000

    🍭🌲 Checkpoint ./komutlar/cüzdan.js:11412426/61:16006939/5 ./rodzen.js:11412426/109

--- komutlar/cüzdan.js
+++ komutlar/cüzdan.js
@@ -1,9 +1,9 @@
 module.exports = {
         name: "cüzdan",
         code: `$author[$userTag[$mentioned[1;yes]]'in Cüzdanı.]
-        $description[**Kullanıcının Cebinde $getServerVar[pbirim]** **$getUserVar[para;[1;]] Kadar Para Var**]
+        $description[**Kullanıcının Cebinde $getServerVar[pbirim]** **$getUserVar[para;$mentioned[1;yes]] Kadar Para Var**]
         $color[RANDOM]
-        $thumbnail[$userAvatar[$mentioned[1;yes]]]
+        $thumbnail[$userAvatar[$mentioned[1;yes]]
         $onlyIf[$isBot[$mentioned[1;yes]]!=true;:x: | Dostum bu bir bot. Botların parası olmaz.]]
         
         
--- rodzen.js
+++ rodzen.js
@@ -175,10 +175,7 @@ $footer[Mixden İnvite Sistemi;$serverIcon]
 $thumbnail[$authorAvatar]
 $suppressErrors[$username Sunucudan Çıktı Kullanıcıyı Kimin Davet Ettiğini Bulamadım]
 $suppressErrors`
-}) 
-bot.onJoined()
-bot.onInviteCreate()
-bot.onInviteDelete()
+})
 bot.command({
 name:"$alwaysExecute",
 code:`
@@ -203,7 +200,6 @@ $thumbnail[$authorAvatar]
 $suppressErrors
 `
 })  
-bot.onLeave
 bot.command({
   name:"$alwaysExecute",
   aliases:['<@!$clientID>', '<@$clientID>'],
@@ -340,13 +336,13 @@ bot.awaitedCommand({
 $image[https://media.discordapp.net/attachments/837255566979235900/864783974807502888/standard_1.gif]
 $addField[$customEmoji[mavisiren] **Destek** (\`1\`);\`destek\`] 
 $addField[$customEmoji[kafa_sallan_anime2] **Müzik Komutları** (\`8\`);\`müzik, play, sıralama, resume, loop, skip, leave, sound\`]
-$addField[$customEmoji[dikkatet] **Koruma Komutlarım** (\`2\`);\`emoji-koruma, link-engel\`]
+$addField[$customEmoji[dikkatet] **Koruma Komutlarım** (\`3\`);\`emoji-koruma, link-engel, küfür-engel\`]
 $addField[$customEmoji[paracklarr] **Ekonomi Komutlarım** (\`14\`);\`ekonomi-yardım, kimlik, cüzdan, günlük, cf, market, para-birimi, para al/ver, para yatır/çek, para-gönder, çalış, satın-al, envanter\`]
 $addField[$customEmoji[giveaway] **Eğlence Komutlarım** (\`8\`);\`howgay, 25miles, palyaço, biden, rte, trump, kaçcm, clyde\`]
 $addField[$customEmoji[asker] **Premium Komutlarım** (\`0\`);\`YAKINDA!\`]
 $addField[$customEmoji[kirmizi_siren] **Ayarlamalı Komutlarım** (\`12\`);\`hgbb-sistemi, mute-sistemi, ticket-sistemi, kayıt-sistemi, otorol, modlog, saas-aç, saas-kapat, tavsiye-log, invite-sistemi, level-sistemi\`]
 $addField[$customEmoji[banned] **Moderasyon Komutlarım** (\`18\`);\`ban, unban, kick, mute, unmute, banlist, herkese-rol-ver, herkesten-rol-al, herkesin-adını-değiştir, nuke, sil, snipe, prefix, hex, otocevap, dm-duyuru, çekiliş, yavaş-mod\`]
-$addField[$customEmoji[astronaut] **Genel Komutlarım** (\`5\`);\`qr, avatar, sunucu-bilgi, afk, tavsiye, istatistik\`]
+$addField[$customEmoji[astronaut] **Genel Komutlarım** (\`6\`);\`qr, avatar, rank, sunucu-bilgi, afk, tavsiye, istatistik\`]
 $addField[**Toplam Komut** (\`$commandsCount\`);\`Moderasyon | Ayarlamalı | Koruma | Genel | Premium | Eğlence | Ekonomi | Destek | Müzik\`]
   $color[$getServerVar[hex]] 
   $thumbnail[$authorAvatar]`
